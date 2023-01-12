41ea43ac94fb9e014a28d72817ce89d961eed064
defan312 <106823339+defan312@users.noreply.github.com>
2023-01-12 19:40:44 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -9,15 +9,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //===> SESUAIKAN DENGAN KEBUTUHAN MU
-global.ownerNumber = ['6285609233482','6285609233482'] //Ganti Jadi Nomer mu Biar Fitur Ownernya Bisa Dipake
-global.Rowner = '6285609233482' //Owner Utama
-global.ownerName = 'Nzrl Afndi' //Ganti Jadi Namamu
-global.packname = 'Subscribe YT' //Ganti Jadi Nama Botmu
-global.author = 'NZRLAFNDI' //Ganti Jadi Namamu
+global.ownerNumber = ['6281321776496'] //Ganti Jadi Nomer mu Biar Fitur Ownernya Bisa Dipake
+global.Rowner = '6281321776496' //Owner Utama
+global.ownerName = 'ᴹᴿ᭄defα͢͢͢ή፝֟͜* ×፝֟͜×' //Ganti Jadi Namamu
+global.packname = 'ᴬᴺᴵᴬ ᴮᴼᵀ' //Ganti Jadi Nama Botmu
+global.author = 'ᴹᴿ᭄defα͢͢͢ή፝֟͜* ×፝֟͜×' //Ganti Jadi Namamu
 global.prefa = ['','.','#','!'] //Ilangin Prefix Yang '' Kalau Gamau No Prefix
 global.sessionName = 'sesi' //Saran Gausah Diganti Lagi
-global.webme = 'https://linktr.ee/NzrlAfndi' //Ganti Jadi Bebas Web Sosmed Mu
-global.sosmed = 'https://www.instagram.com/_nzrlafndi' //Ganti Jadi Web Sosmed Mu
+global.webme = 'https://saweria.co/defan' //Ganti Jadi Bebas Web Sosmed Mu
+global.sosmed = 'https://instagram.com/defan_sugiarto' //Ganti Jadi Web Sosmed Mu
 
 //====> SARAN GAUSAH DIGANTI SIH DAH PAS SEMUA
 global.limitAwal = {
